d25f74d33b9bd0ccfaec1fab58c7d467c3484389
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-07-05 01:03:26 +0200

    error page updated

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,12 @@
 import '../../App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ErrorPage } from '../ErrorPage';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Landing } from '../Landing';
 import { Login } from '../Login';
 import { Signup } from '../Signup';
 import { Welcome } from '../Welcome';
+import { ErrorPage } from '../ErrorPage';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <Route path="/welcome" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/*" element={<ErrorPage />} />
+        <Route path="/*" element={<ErrorPage />}/>
       </Routes>
 
       <Footer />
