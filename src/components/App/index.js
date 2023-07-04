40dac1ee1595c5444da9806e50c52c8042f88c66
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-07-04 18:01:37 +0200

    landing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,15 @@
 import '../../App.css';
 import { Header } from '../Header';
+import { Landing } from '../Landing';
 
 function App() {
   return (
     <div>
+      {/* Header component */}
       <Header />
+
+      {/* Landing component */}
+      <Landing />
     </div>
   );
 }
