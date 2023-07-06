b9fd36db787a6662d2375a0ba2b480a7828b8c33
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-07-06 20:35:47 +0200

    levels & progressbar Quiz created

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,16 +1,28 @@
 // Comme on a fait que des composants de type fonction, on va faire une classe ici pour changer
 import { Component } from 'react';
+import { Levels } from '../Levels';
+import { ProgressBar } from '../ProgressBar';
 
 export class Quiz extends Component {
     
     render() {
 
         // destructuring
-        const { pseudo } = this.props.userData;
+        // const { pseudo } = this.props.userData;
 
         return(
             <div>
-                <h2>Pseudo: {pseudo}</h2>
+                {/* <h2>Pseudo: {pseudo}</h2> */}
+
+                <Levels/>
+                <ProgressBar/>
+
+                <h2>Notre question quiz</h2>
+                <p className="answerOptions">Question 1</p>
+                <p className="answerOptions">Question 2</p>
+                <p className="answerOptions">Question 3</p>
+                <p className="answerOptions">Question 4</p>
+                <button className="btnSubmit">Suivant</button>
             </div>
         )
     }
